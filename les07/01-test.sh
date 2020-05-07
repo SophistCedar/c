21984f536ecf2535db6ef99f0d2f99998d65860b
WeinanCai <1832721@tongji.edu.cn>
2020-05-06 23:06:39 -0700

    second update

---
+++ les07/01-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "hello world!"
+url="http://www.runoob.com"
+# #! 是一个约定的标记，它告诉系统这个脚本需要什么解释器来执行，即使用哪一种 Shell。
+# echo 命令用于向窗口输出文本。
+# 作为可执行程序
+# 将上面的代码保存为 test.sh，并 cd 到相应目录：
+# chmod +x ./test.sh  #使脚本具有执行权限
+# ./test.sh  #执行脚本
---
+++ les09/client.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+const char *ip = "192.168.14.128";
+const uint16_t port = 1234;
+int main(){
+    //创建套接字
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1){
+		printf("socket error!\n");
+		return 0;
+	}
+    //向服务器（特定的IP和端口）发起请求
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;  //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr(ip);  //具体的IP地址
+    serv_addr.sin_port = htons(port);  //端口
+    int ret = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (ret != 0){
+        printf("connect error!\n");
+        return 0;
+    }
+    //读取服务器传回的数据
+    char buffer[255];
+    char str[] = "Hello server!";
+    while(1){
+        ret = recv(sock, buffer, sizeof(buffer),0);
+        if(ret < 0){
+            printf("recv error!\n");
+            return 0;
+        }
+        printf("recv %d bytes: %s\n",ret,buffer);  
+        
+        // ret = send(sock, str, sizeof(str),0);
+        // if (ret < 0){
+        //     printf("send error!\n");
+        //     return 0;
+        // }
+        // printf("send %d bytes: %s\n",ret,str);  
+
+        sleep(1);
+    }
+    //关闭套接字
+    close(sock);
+    return 0;
+}
